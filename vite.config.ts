1d85630dea2023858f8a10b0a17ab8bbad549ac4
aiprojek <71024676+aiprojek@users.noreply.github.com>
2026-02-11 11:57:35 +0700

    feat(build): Configure Vite for Tauri integration
    Integrate Vite with Tauri by enabling module support and configuring server and build options specific to Tauri development. This includes setting a fixed server port, using appropriate environment prefixes, and tailoring build targets and minification for different platforms and debug builds.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Vite options tailored for Tauri development
+  clearScreen: false,
+  server: {
+    port: 5173,
+    strictPort: true, // Tauri expects a fixed port, fail if that port is not available
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
     outDir: 'dist',
+    // Tauri supports es2021
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 });
